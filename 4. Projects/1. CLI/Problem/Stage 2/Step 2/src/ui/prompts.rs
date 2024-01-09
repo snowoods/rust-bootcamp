18bed4e802b63d9987376f53ac169e710dea8740
nbc <nbc@snowoods.com>
2024-01-09 15:28:25 +0900

    CLI prompt 추가

--- 4. Projects/1. CLI/Problem/Stage 2/Step 2/src/ui/prompts.rs
+++ 4. Projects/1. CLI/Problem/Stage 2/Step 2/src/ui/prompts.rs
@@ -21,21 +21,91 @@ impl Prompts {
 }
 
 fn create_epic_prompt() -> Epic {
-    todo!();
+    println!("----------------------------");
+
+    println!("Epic Name: ");
+
+    let epic_name = get_user_input();
+
+    println!("Epic Description: ");
+
+    let epic_desc = get_user_input();
+
+    let epic = Epic::new(epic_name.trim().to_owned(), epic_desc.trim().to_owned());
+
+    epic
 }
 
 fn create_story_prompt() -> Story {
-    todo!();
+    println!("----------------------------");
+
+    println!("Story Name: ");
+
+    let story_name = get_user_input();
+
+    println!("Story Description: ");
+
+    let story_desc = get_user_input();
+
+    let story = Story::new(story_name.trim().to_owned(), story_desc.trim().to_owned());
+
+    story
 }
 
 fn delete_epic_prompt() -> bool {
-    todo!();
+    println!("----------------------------");
+
+    println!("Are you sure you want to delete this epic? All stories in this epic will also be deleted [Y/n]: ");
+
+    let input = get_user_input();
+
+    if input.trim().eq("Y") {
+        return true;
+    }
+
+    false
 }
 
 fn delete_story_prompt() -> bool {
-    todo!();
+    println!("----------------------------");
+
+    println!("Are you sure you want to delete this story? [Y/n]: ");
+
+    let input = get_user_input();
+
+    if input.trim().eq("Y") {
+        return true;
+    }
+
+    false
 }
 
 fn update_status_prompt() -> Option<Status> {
-    todo!();
+    println!("----------------------------");
+
+    println!("New Status (1 - OPEN, 2 - IN-PROGRESS, 3 - RESOLVED, 4 - CLOSED): ");
+
+    let status = get_user_input();
+
+    let status = status.trim().parse::<u8>();
+
+    if let Ok(status) = status {
+        match status {
+            1 => {
+                return Some(Status::Open);
+            }
+            2 => {
+                return Some(Status::InProgress);
+            }
+            3 => {
+                return Some(Status::Resolved);
+            }
+            4 => {
+                return Some(Status::Closed);
+            }
+            _ => return None
+        }
+    }
+
+    None
 }
