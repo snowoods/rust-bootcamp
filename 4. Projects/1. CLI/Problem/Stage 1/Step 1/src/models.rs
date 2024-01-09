dc9c6a100ed528080b43f4dfd605804a5e1e306b
nbc <nbc@snowoods.com>
2024-01-09 11:41:26 +0900

    Update models.rs

--- 4. Projects/1. CLI/Problem/Stage 1/Step 1/src/models.rs
+++ 4. Projects/1. CLI/Problem/Stage 1/Step 1/src/models.rs
@@ -4,15 +4,16 @@ pub enum Status {
     // TODO: add fields (make sure the fields are public)
     Open,
     InProgress,
+    Resolved,
     Closed,
 }
 
 pub struct Epic {
     // TODO: add fields (make sure the fields are public)
-    name: String,
-    description: String,
-    status: Status,
-    stories: Vec<u32>,
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+    pub stories: Vec<u32>,
 }
 
 impl Epic {
@@ -22,16 +23,16 @@ impl Epic {
             name,
             description,
             status: Status::Open,
-            stories: vec![]            
+            stories: vec![]
         }
     }
 }
 
 pub struct Story {
     // TODO: add fields (make sure the fields are public)
-    name: String,
-    description: String,
-    status: Status,
+    pub name: String,
+    pub description: String,
+    pub status: Status,
 }
 
 impl Story {
@@ -45,8 +46,8 @@ impl Story {
     }
 }
 
+// This struct represents the entire db state which includes the last_item_id, epics, and stories
 pub struct DBState {
-    // This struct represents the entire db state which includes the last_item_id, epics, and stories
     // TODO: add fields (make sure the fields are public)
     pub last_item_id: String,
     pub epics: HashMap<u32, Epic>,
