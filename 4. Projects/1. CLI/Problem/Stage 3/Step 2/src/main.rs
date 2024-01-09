58a8cee586d57e4172d81af020943cafbcd79bb5
nbc <nbc@snowoods.com>
2024-01-09 17:00:32 +0900

    Program Loop 추가.

--- 4. Projects/1. CLI/Problem/Stage 3/Step 2/src/main.rs
+++ 4. Projects/1. CLI/Problem/Stage 3/Step 2/src/main.rs
@@ -15,15 +15,43 @@ use navigator::*;
 
 fn main() {
     // TODO: create database and navigator
+    let db = Rc::new(JiraDatabase::new("./data/db.json".to_owned()));
+    let mut navigator = Navigator::new(Rc::clone(&db));
     
     loop {
         clearscreen::clear().unwrap();
 
         // TODO: implement the following functionality:
         // 1. get current page from navigator. If there is no current page exit the loop.
-        // 2. render page
-        // 3. get user input
-        // 4. pass input to page's input handler
-        // 5. if the page's input handler returns an action let the navigator process the action
+        if let Some(page) = navigator.get_current_page() {
+            
+            // 2. render page
+            if let Err(error) = page.draw_page() {
+                println!("Error rendering page: {}\nPress any key to continue...", error);
+                wait_for_key_press();
+            }
+
+            // 3. get user input
+            let user_input = get_user_input();
+
+            // 4. pass input to page's input handler
+            match page.handle_input(user_input.trim()) {
+                Err(error) => {
+                    println!("Error getting user input: {}\nPress any key to continue...", error);
+                    wait_for_key_press();
+                },
+                // 5. if the page's input handler returns an action let the navigator process the action
+                Ok(action) => {
+                    if let Some(action) = action {
+                        if let Err(error) = navigator.handle_action(action) {
+                            println!("Error handling processing user input: {}\nPress any key to continue...", error);
+                            wait_for_key_press();
+                        }
+                    }
+                }
+            }
+        } else {
+            break;
+        }
     }
 }
