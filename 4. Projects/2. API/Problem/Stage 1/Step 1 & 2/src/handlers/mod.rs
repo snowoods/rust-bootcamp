4744ff224c8a90470befa6cd721c33ae73c448a5
nbc <nbc@snowoods.com>
2024-01-11 16:31:33 +0900

    API Question and Answer CRUD 추가.

--- 4. Projects/2. API/Problem/Stage 1/Step 1 & 2/src/handlers/mod.rs
+++ 4. Projects/2. API/Problem/Stage 1/Step 1 & 2/src/handlers/mod.rs
@@ -8,19 +8,33 @@ use crate::models::*;
 pub async fn create_question(
     question: Json<Question>,
 ) -> Json<QuestionDetail> {
-    todo!()
+    Json (
+        QuestionDetail {
+            question_uuid: "question_uuid".to_owned(),
+            title: "title".to_owned(),
+            description: "deseription".to_owned(),
+            created_at: "created_at".to_owned()
+        }
+    )
 }
 
 #[get("/questions")]
 pub async fn read_questions() -> Json<Vec<QuestionDetail>> {
-    todo!()
+    Json (
+        vec![QuestionDetail {
+            question_uuid: "question_uuid".to_owned(),
+            title: "title".to_owned(),
+            description: "description".to_owned(),
+            created_at: "created_at".to_owned()
+        }]
+    )
 }
 
 #[delete("/question", data = "<question_uuid>")]
 pub async fn delete_question(
     question_uuid: Json<QuestionId>
 ) {
-    todo!()
+    // ...
 }
 
 // ---- CRUD for Answers ----
@@ -29,13 +43,45 @@ pub async fn delete_question(
 //       The handler function should be called `create_answer`.
 //       
 //       hint: this function should look very similar to the create_question function above
+#[post("/answer", data = "<answer>")]
+pub async fn create_answer(
+    answer: Json<Answer>
+) -> Json<AnswerDetail> {
+    Json (
+        AnswerDetail {
+            answer_uuid: "answer_uuid".to_owned(),
+            question_uuid: "question_uuid".to_owned(),
+            content: "content".to_owned(),
+            created_at: "created_at".to_owned()
+        }
+    )
+}
 
 // TODO: Create a GET route to /answers which accepts an `QuestionId` and returns a vector of `AnswerDetail` as JSON.
 //       The handler function should be called `read_answers`.
 //       
 //       hint: this function should look very similar to the read_questions function above
+#[get("/answers", data = "<question_uuid>")]
+pub async fn read_answers(
+    question_uuid: Json<QuestionId>
+) -> Json<Vec<AnswerDetail>> {
+    Json (
+        vec![AnswerDetail {
+            answer_uuid: "answer_uuid".to_owned(),
+            question_uuid: "question_uuid".to_owned(),
+            content: "content".to_owned(),
+            created_at: "created_at".to_owned()
+        }]
+    )
+}
 
 // TODO: Create a DELETE route to /answer which accepts an `AnswerId` and does not return anything.
 //       The handler function should be called `delete_answer`.
 //       
 //       hint: this function should look very similar to the delete_question function above
+#[delete("/answer", data = "<answer_uuid>")]
+pub async fn delete_answer(
+    answer_uuid: Json<AnswerId>
+) {
+    // ...
+}
--- 4. Projects/2. API/Problem/Stage 1/Step 1 & 2/src/models.rs
+++ 4. Projects/2. API/Problem/Stage 1/Step 1 & 2/src/models.rs
@@ -4,6 +4,8 @@ use serde::{Deserialize, Serialize};
 pub struct Question {
     // TODO: add a public `title` field of type String
     // TODO: add a public `description` field of type String
+    pub title: String,
+    pub description: String,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
@@ -12,11 +14,19 @@ pub struct QuestionDetail {
     // TODO: add a public `title` field of type String
     // TODO: add a public `description` field of type String
     // TODO: add a public `created_at` field of type String
+    pub question_uuid: String,
+    pub title: String,
+    pub description: String,
+    pub created_at: String,
 }
 
 // TODO: create a struct called `QuestionId`
 //       derive the following traits: Serialize, Deserialize
 //       add a public `question_uuid` field of type String
+#[derive(Serialize, Deserialize)]
+pub struct QuestionId {
+    pub question_uuid: String,
+}
 
 // ----------
 
@@ -24,6 +34,11 @@ pub struct QuestionDetail {
 //       derive the following traits: Serialize, Deserialize
 //       add a public `question_uuid` field of type String
 //       add a public `content` field of type String
+#[derive(Serialize, Deserialize)]
+pub struct Answer {
+    pub question_uuid: String,
+    pub content: String,
+}
 
 // TODO: create a struct called `AnswerDetail`
 //       derive the following traits: Serialize, Deserialize, Debug, Clone, PartialEq
@@ -31,7 +46,18 @@ pub struct QuestionDetail {
 //       add a public `question_uuid` field of type String
 //       add a public `content` field of type String
 //       add a public `created_at` field of type String
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct AnswerDetail {
+    pub answer_uuid: String,
+    pub question_uuid: String,
+    pub content: String,
+    pub created_at: String,
+}
 
 // TODO: create a struct called `AnswerId`
 //       derive the following traits: Serialize, Deserialize
 //       add a public `answer_uuid` field of type String
+#[derive(Serialize, Deserialize)]
+pub struct AnswerId {
+    pub answer_uuid: String
+}
