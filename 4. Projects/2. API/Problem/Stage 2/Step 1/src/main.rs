9722088e8319aa7323350e43c46c8b054bd463a8
nbc <nbc@snowoods.com>
2024-01-12 15:04:11 +0900

    Update main.rs
    업데이트 후 실행 명령어.
    
    $ cargo install sqlx-cli
    $ sqlx migrate run
    $ sqlx migrate revert
    
    $ cargo watch -q -c -w src/ -x run

--- 4. Projects/2. API/Problem/Stage 2/Step 1/src/main.rs
+++ 4. Projects/2. API/Problem/Stage 2/Step 1/src/main.rs
@@ -2,6 +2,12 @@
 extern crate rocket;
 
 // TODO: import log, pretty_env_logger, dotenv, and PgPoolOptions
+#[macro_use]
+extern crate log;
+extern crate pretty_env_logger;
+
+use dotenvy::dotenv;
+use sqlx::postgres::PgPoolOptions;
 
 mod cors;
 mod handlers;
@@ -13,7 +19,9 @@ use handlers::*;
 #[launch]
 async fn rocket() -> _ {
     // TODO: Initialize pretty_env_logger
+    pretty_env_logger::init();
     // TODO: Initialize dotenv
+    dotenv().ok();
 
     // Create a new PgPoolOptions instance with a maximum of 5 connections.
     // Use dotenv to get the database url. 
@@ -21,14 +29,24 @@ async fn rocket() -> _ {
     // error occurs at this stage the server should be terminated. 
     // See examples on GitHub page: https://github.com/launchbadge/sqlx
     // let pool = todo!();
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be set."))
+        .await
+        .expect("Failed to create Postgres connection pool!");
 
     // Using slqx, execute a SQL query that selects all questions from the questions table.
     // Use the `unwrap` or `expect` method to handle errors. This is just some test code to
     // make sure we can connect to the database.  
     // let recs = todo!();
+    let recs = sqlx::query!("SELECT * FROM questions")
+        .fetch_all(&pool)
+        .await
+        .unwrap();
 
     info!("********* Question Records *********");
     // TODO: Log recs with debug formatting using the info! macro
+    info!("{:?}", recs);
 
     rocket::build()
         .mount(
